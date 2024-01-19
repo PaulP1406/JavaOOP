208140fa7eeb134e768139555626cd66fbd4b429
Duc Duy Pham <ddp4@sfu.ca>
2024-01-19 13:51:06 -0800

    Added Instructor class, implemented all the methods needed

---
+++ Instructor.java
@@ -0,0 +1,23 @@
+public class Instructor extends Person {
+    private String eId; // Short for employee's ID
+
+    // Constructor for Instructor
+    public Instructor(String name, String email, String employeeId) {
+        super(name, email); // Calling the constructor for name and email from Person class
+        this.eId = employeeId;
+    }
+
+    // Getter and Setter for eId
+    public String getEid() {
+        return eId;
+    }
+
+    public void setEid(String employeeId) {
+        this.eId = employeeId;
+    }
+
+    // Get ID method implementation
+    public String getId() {
+        return getEid();
+    }
+}
