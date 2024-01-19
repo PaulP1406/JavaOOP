6d3aaab7ed35c88c0b9f832f9685331d5f92ca2d
Duc Duy Pham <ddp4@sfu.ca>
2024-01-19 13:59:45 -0800

    Added Student class and implemented all required methods

--- Instructor.java
+++ Instructor.java
@@ -1,7 +1,7 @@
 public class Instructor extends Person {
     private String eId; // Short for employee's ID
 
-    // Constructor for Instructor
+    // Constructor
     public Instructor(String name, String email, String employeeId) {
         super(name, email); // Calling the constructor for name and email from Person class
         this.eId = employeeId;
---
+++ Student.java
@@ -0,0 +1,34 @@
+public class Student extends Person {
+    private String sID; // Short for student's ID
+    private String grade; // Grade is in letter grade, from A+ to F
+
+    // Constructor
+    public Student(String name, String email, String studentId, String grade) {
+        super(name, email); // Calling the constructor for name and email from Person class
+        this.sID = studentId;
+        this.grade = grade;
+    }
+
+    // Getter and setter for sID
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String studentId) {
+        this.sID = studentId;
+    }
+
+    // Getter and setter for grade
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    // Get ID method implementation
+    public String getId() {
+        return getsID();
+    }
+}
