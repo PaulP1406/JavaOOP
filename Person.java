e9e76234d7655a51ab15491e57c9c3c48f8ff695
Duc Duy Pham <ddp4@sfu.ca>
2024-01-19 13:37:45 -0800

    Created assignment 1 directory, added Person class

---
+++ Person.java
@@ -0,0 +1,30 @@
+public abstract class Person {
+    protected String name;
+    protected String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Abstract method to be filled in by subclasses Intructors and Student
+    public abstract String getId();
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
