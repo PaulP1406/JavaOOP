18f227d5261fa97bbf205de55e71f6fde7af69d9
Duc Duy Pham <ddp4@sfu.ca>
2024-02-17 23:05:44 -0800

    Added Javadoc for Room and Direction files

--- Assignment2/maze-game/src/main/java/com/SimonFraserUniversity/DucDuyPham/Direction.java
+++ Assignment2/maze-game/src/main/java/com/SimonFraserUniversity/DucDuyPham/Direction.java
@@ -1,5 +1,9 @@
 package com.SimonFraserUniversity.DucDuyPham;
 
+/**
+ * The Direction enum represents the four directions North South East and West that are
+ * used to define the sides of a room.
+ */
 public enum Direction {
     North, South, East, West
 }
--- Assignment2/maze-game/src/main/java/com/SimonFraserUniversity/DucDuyPham/Room.java
+++ Assignment2/maze-game/src/main/java/com/SimonFraserUniversity/DucDuyPham/Room.java
@@ -1,5 +1,9 @@
 package com.SimonFraserUniversity.DucDuyPham;
 
+/**
+ * Represents a room in the maze. Each room has four sides that can be either a
+ * wall or a door.
+ */
 public class Room extends MapSite {
     protected int roomNum;
     protected static int roomCount = 1;
@@ -8,16 +12,26 @@ public class Room extends MapSite {
     protected MapSite eastSide;
     protected MapSite westSide;
 
+    /**
+     * Create a Room and assigns a room number to it.
+     */
     public Room() {
         this.roomNum = roomCount++;
         System.out.println("Creating " + this.toString());
     }
 
+    /**
+     * Sets one of the sides of the room to the specified MapSite.
+     *
+     * @param direction The direction of the side of the room being set (North,
+     *                  South, East, West).
+     * @param site      The MapSite element to modify a side in a certain direction
+     */
     public void setSide(Direction direction, MapSite site) {
         switch (direction) {
             case North:
                 northSide = site;
-                break; // Fixed: Added break statements
+                break;
             case South:
                 southSide = site;
                 break;
@@ -31,15 +45,28 @@ public class Room extends MapSite {
         System.out.println("Setting " + direction + " side of " + this.toString() + " to " + site);
     }
 
+    /**
+     * Getter method for returning the room number of the room
+     *
+     * @return The room number assigned to this room.
+     */
     public int getRoomNum() {
         return this.roomNum;
     }
 
+    /**
+     * Priting a message to describe the method entering the room
+     */
     @Override
     public void enter() {
         System.out.println("Entering room " + roomNum);
     }
 
+    /**
+     * Returns a string representation of the room element, with the room number
+     * 
+     * @return A string that represents the room number.
+     */
     @Override
     public String toString() {
         return "Room #" + roomNum;
