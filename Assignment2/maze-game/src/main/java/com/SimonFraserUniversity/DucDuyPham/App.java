0387f51ee3968e9ffb3302fc9018225e3283e76b
Duc Duy Pham <ddp4@sfu.ca>
2024-02-17 22:36:52 -0800

    fixes and compiled the project using maven

--- Assignment2/maze-game/src/main/java/com/SimonFraserUniversity/DucDuyPham/App.java
+++ Assignment2/maze-game/src/main/java/com/SimonFraserUniversity/DucDuyPham/App.java
@@ -2,13 +2,13 @@ package com.SimonFraserUniversity.DucDuyPham;
 
 public class App {
     public static void main(String[] args) {
-         MazeFactory factory = new MazeFactory();
-         MazeGame game = new MazeGame();
-         game.createMaze(factory);
+        MazeFactory factory = new MazeFactory();
+        MazeGame game = new MazeGame();
+        game.createMaze(factory);
 
-        //EnchantedMazeFactory factory2 = new EnchantedMazeFactory();
-        //MazeGame game2 = new MazeGame();
-        //game2.createMaze(factory2);
+        // EnchantedMazeFactory factory2 = new EnchantedMazeFactory();
+        // MazeGame game2 = new MazeGame();
+        // game2.createMaze(factory2);
 
     }
 }
